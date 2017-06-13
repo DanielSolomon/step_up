1823d6cf14958ed84db86d0d8b4faefba54c9381
DanielSolomon <DanielSolomon94.ds@gmail.com>
2017-06-13 21:31:55 +0300

    Now you can run fix displays with wanted resolution as input and not hard coded.

--- scripts/fix_display.sh
+++ scripts/fix_display.sh
@@ -1,9 +1,20 @@
+#set -x
+
 run_sudo()
 {
     echo "qwerty" | sudo -S $@
 }
 
-# display resolution shit.
-run_sudo cvt 3840 2160 60
-run_sudo xrandr --newmode 3840x2160  712.75  3840 4160 4576 5312  2160 2163 2168 2237 -hsync +vsync
-run_sudo xrandr --addmode Virtual1 3840x2160
+mode=1
+while [[ ! -z $1 ]]
+do
+    echo "$1"
+    w=$(echo "$1" | cut -d'x' -f1)
+    h=$(echo "$1" | cut -d'x' -f2)
+    # display resolution shit.
+    run_sudo cvt $w $h 60
+    run_sudo xrandr --newmode $1  712.75  $w 4160 4576 5312  $h 2163 2168 2237 -hsync +vsync
+    run_sudo xrandr --addmode Virtual$mode $1
+    shift
+    mode=$((mode + 1))
+done
